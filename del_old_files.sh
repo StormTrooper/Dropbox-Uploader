31563677953b2baa608fb1f62d1a0fd3af7b77fe
Greg McCarthy <greg@gjmccarthy.co.uk>
2014-03-23 09:24:15 +0000

    Fixed error in script

--- del_old_files.sh
+++ del_old_files.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
-days=30
+days=7
 db_location="/root/Dropbox-Uploader"
 
 folder="home/backup/data"
-$db_location/dropbox_uploader.sh list $folder /tmp/files.txt
+$db_location/dropbox_uploader.sh list $folder > /tmp/files.txt
+$db_location/del_dropbox.sh $folder $days
+
+
+folder="home/backup/config"
+$db_location/dropbox_uploader.sh list $folder > /tmp/files.txt
+$db_location/del_dropbox.sh $folder $days
+
+folder="home/backup/database"
+$db_location/dropbox_uploader.sh list $folder > /tmp/files.txt
 $db_location/del_dropbox.sh $folder $days
 
 
